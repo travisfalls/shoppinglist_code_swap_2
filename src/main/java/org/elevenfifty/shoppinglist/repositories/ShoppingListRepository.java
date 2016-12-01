ba89dfdfc83e21550cc83a20283b256364bf2a3d
Spencer Haggard <spencer.timekeeper99@gmail.com>
2016-12-01 12:14:13 -0500

    Add list controllers

--- src/main/java/org/elevenfifty/shoppinglist/repositories/ShoppingListRepository.java
+++ src/main/java/org/elevenfifty/shoppinglist/repositories/ShoppingListRepository.java
@@ -1,10 +1,13 @@
 package org.elevenfifty.shoppinglist.repositories;
 
 import org.elevenfifty.shoppinglist.beans.ShoppingList;
+import org.elevenfifty.shoppinglist.beans.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ShoppingListRepository extends CrudRepository<ShoppingList, Long> {
+
+	Object findAllByUser(User u);
 
 }
