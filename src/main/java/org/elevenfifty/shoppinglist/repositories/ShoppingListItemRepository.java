6cc4684d8551d3ece9ebd87649f906f6cc8fe501
Ethan Ransdell <ethan.ransdell@live.com>
2016-12-05 10:31:34 -0500

    added ability to edit note, and add an item with a note

--- src/main/java/org/elevenfifty/shoppinglist/repositories/ShoppingListItemRepository.java
+++ src/main/java/org/elevenfifty/shoppinglist/repositories/ShoppingListItemRepository.java
@@ -1,6 +1,6 @@
 package org.elevenfifty.shoppinglist.repositories;
 
-import java.util.Set;
+import java.util.ArrayList;
 
 import org.elevenfifty.shoppinglist.beans.ShoppingListItem;
 import org.springframework.data.repository.CrudRepository;
@@ -9,5 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ShoppingListItemRepository extends CrudRepository<ShoppingListItem, Long> {
 
-	Set<ShoppingListItem> findByShoppingListId(long shoppingListId);
+	ArrayList<ShoppingListItem> findByShoppingListId(long shoppingListId);
+
 }
